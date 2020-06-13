2228dd2bb9cc51339f3b973c2c7a2c2b7fc94c01
Karan Chothani <karanchothani1432/2gmail.com>
2020-06-13 22:18:29 +0530

    bug fixed

---
+++ lib/Tabs/ListTab.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ListTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Center(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              child: new Text(
+                "LIST_PAGE",
+                style: Theme.of(context).textTheme.display1,
+              ),
+            ),
+            Container(
+              child: new RaisedButton(
+                color: Colors.purple,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'Back To Home',
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
