5ecfbb0dab8783be4a3b128c5c6519f5ce05642c
Karan Chothani <karanchothani1432/2gmail.com>
2020-07-05 16:10:02 +0530

    bug fixed

--- lib/main.dart
+++ lib/main.dart
@@ -178,201 +178,175 @@ class _HomeState extends State<Home> {
               //width: 1000.0,
               child: new Stack(
                 children: <Widget>[
-                  new Stack(
-                    children: <Widget>[
-                      new Align(
-                        child: MatrixGestureDetector(
-                          onMatrixUpdate: (m, tm, sm, rm) {
-                            notifier1.value = m;
-                          },
-                          // shouldRotate: false,
-                          // shouldScale: true,
-                          // shouldTranslate: false,
-                          child: AnimatedBuilder(
-                            animation: notifier1,
-                            builder: (ctx, child) {
-                              return Transform(
-                                transform: notifier1.value,
-                                child: Stack(
-                                  children: <Widget>[
-                                    Align(
-                                      alignment: Alignment(0.0,-0.85),
-                                      child: Container(
-                                        width: 100,
-                                        height: 100,
-                                        color: Colors.green[100],
-                                        //color: Colors.purple,
-                                        child: DragTarget(
-                                          builder: (context, List<int> candidateData, rejectedData) {
-                                            print(candidateData);
-                                            return Center(child: Image(image: AssetImage('images/part1.jpg'),height: h,width: w,),);
-                                          },
-                                        ),
-                                      ),
-                                    ),
-                                  ],
+                  new MatrixGestureDetector(
+                    onMatrixUpdate: (m, tm, sm, rm) {
+                      notifier1.value = m;
+                    },
+                    // shouldRotate: false,
+                    // shouldScale: true,
+                    // shouldTranslate: false,
+                    child: AnimatedBuilder(
+                      animation: notifier1,
+                      builder: (ctx, child) {
+                        return Transform(
+                          transform: notifier1.value,
+                          child: Stack(
+                            children: <Widget>[
+                              Align(
+                                alignment: Alignment(0.0,-0.85),
+                                child: Container(
+                                  width: w,
+                                  height: h,
+                                  color: Colors.green[100],
+                                  //color: Colors.purple,
+                                  child: DragTarget(
+                                    builder: (context, List<int> candidateData, rejectedData) {
+                                      print(candidateData);
+                                      return Center(child: Image(image: AssetImage('images/part1.jpg'),height: h,width: w,),);
+                                    },
+                                  ),
                                 ),
-                              );
-                            },
+                              ),
+                            ],
                           ),
-                        ),
-                      ),
-                    ]
-                  ),
-                  new Stack(
-                      children: <Widget>[
-                        new Align(
-                          child: MatrixGestureDetector(
-                            onMatrixUpdate: (m, tm, sm, rm) {
-                              notifier2.value = m;
-                            },
-                            // shouldRotate: false,
-                            // shouldScale: true,
-                            // shouldTranslate: false,
-                            child: AnimatedBuilder(
-                              animation: notifier2,
-                              builder: (ctx, child) {
-                                return Transform(
-                                  transform: notifier2.value,
-                                  child: Stack(
-                                    children: <Widget>[
-                                      Align(
-                                        alignment: Alignment(0.0,-0.35),
-                                        child: Container(
-                                          width: 100.0,
-                                          height: 100.0,
-                                          //color: Colors.purple,
-                                          child: DragTarget(
-                                            builder: (context, List<int> candidateData, rejectedData) {
-                                              print(candidateData);
-                                              return Center(child: Image(image: AssetImage('images/part2.jpg'),),);
-                                            },
-                                          ),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                );
-                              },
-                            ),
-                          ),
-                        ),
-                      ]
-                  ),
-                  new Stack(
-                      children: <Widget>[
-                        new Align(
-                          child: MatrixGestureDetector(
-                            onMatrixUpdate: (m, tm, sm, rm) {
-                              notifier3.value = m;
-                            },
-                            // shouldRotate: false,
-                            // shouldScale: true,
-                            // shouldTranslate: false,
-                            child: AnimatedBuilder(
-                              animation: notifier3,
-                              builder: (ctx, child) {
-                                return Transform(
-                                  transform: notifier3.value,
-                                  child: Stack(
-                                    children: <Widget>[
-                                      Align(
-                                        alignment: Alignment(0.0,0.15),
-                                        child: Container(
-                                          width: 100.0,
-                                          height: 100.0,
-                                          //color: Colors.purple,
-                                          child: DragTarget(
-                                            builder: (context, List<int> candidateData, rejectedData) {
-                                              print(candidateData);
-                                              return Center(child: Image(image: AssetImage('images/part3.jpg'),),);
-                                            },
-                                          ),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                );
-                              },
-                            ),
-                          ),
-                        ),
-                      ]
-                  ),
-                  new Stack(
-                      children: <Widget>[
-                        new Align(
-                          child: MatrixGestureDetector(
-                            onMatrixUpdate: (m, tm, sm, rm) {
-                              notifier4.value = m;
-                            },
-                            // shouldRotate: false,
-                            // shouldScale: true,
-                            // shouldTranslate: false,
-                            child: AnimatedBuilder(
-                              animation: notifier4,
-                              builder: (ctx, child) {
-                                return Transform(
-                                  transform: notifier4.value,
-                                  child: Stack(
-                                    children: <Widget>[
-                                      Align(
-                                        alignment: Alignment(0.0,0.65),
-                                        child: Container(
-                                          width: 100.0,
-                                          height: 100.0,
-                                          //color: Colors.purple,
-                                          child: DragTarget(
-                                            builder: (context, List<int> candidateData, rejectedData) {
-                                              print(candidateData);
-                                              return Center(child: Image(image: AssetImage('images/part4.jpg'),),);
-                                            },
-                                          ),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                );
-                              },
-                            ),
-                          ),
-                        ),
-                      ]
-                  ),
-                  new Align(
-                    child: new SpeedDial(
-                      animatedIcon: AnimatedIcons.menu_arrow,
-                      animatedIconTheme: IconThemeData(size: 24.0),
-                      closeManually: false,
-                      marginRight: 21.5,
-                      marginBottom: 23.5,
-                      curve: Curves.easeIn,
-                      overlayColor: Colors.black,
-                      overlayOpacity: 0.3,
-                      foregroundColor: Colors.white,
-                      backgroundColor: Colors.purple,
-                      //tooltip: "Speed Dial123",
-                      //closeManually: true,
-                      children: [
-                        SpeedDialChild(
-                            child: Icon(Icons.zoom_in, size: 30,),
-                            foregroundColor: Colors.white,
-                            backgroundColor: Colors.purple,
-                            label: "Zoom In",
-                            labelStyle: TextStyle(fontSize: 14.5),
-                            onTap: zoomin,
-                            ),
-                        SpeedDialChild(
-                            child: Icon(Icons.zoom_out, size: 30,),
-                            foregroundColor: Colors.white,
-                            backgroundColor: Colors.purple,
-                            label: "Zoom Out",
-                            labelStyle: TextStyle(fontSize: 14.5),
-                            onTap: zoomout,
-                            ),
-                      ],
+                        );
+                      },
                     ),
-                  ), //Speed dial
+                  ),
+                  new MatrixGestureDetector(
+                    onMatrixUpdate: (m, tm, sm, rm) {
+                      notifier2.value = m;
+                    },
+                    // shouldRotate: false,
+                    // shouldScale: true,
+                    // shouldTranslate: false,
+                    child: AnimatedBuilder(
+                      animation: notifier2,
+                      builder: (ctx, child) {
+                        return Transform(
+                          transform: notifier2.value,
+                          child: Stack(
+                            children: <Widget>[
+                              Align(
+                                alignment: Alignment(0.0,-0.35),
+                                child: Container(
+                                  width: h,
+                                  height: w,
+                                  //color: Colors.purple,
+                                  child: DragTarget(
+                                    builder: (context, List<int> candidateData, rejectedData) {
+                                      print(candidateData);
+                                      return Center(child: Image(image: AssetImage('images/part2.jpg'),height: h,width: w,),);
+                                    },
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                  new MatrixGestureDetector(
+                    onMatrixUpdate: (m, tm, sm, rm) {
+                      notifier3.value = m;
+                    },
+                    // shouldRotate: false,
+                    // shouldScale: true,
+                    // shouldTranslate: false,
+                    child: AnimatedBuilder(
+                      animation: notifier3,
+                      builder: (ctx, child) {
+                        return Transform(
+                          transform: notifier3.value,
+                          child: Stack(
+                            children: <Widget>[
+                              Align(
+                                alignment: Alignment(0.0,0.15),
+                                child: Container(
+                                  width: w,
+                                  height: h,
+                                  //color: Colors.purple,
+                                  child: DragTarget(
+                                    builder: (context, List<int> candidateData, rejectedData) {
+                                      print(candidateData);
+                                      return Center(child: Image(image: AssetImage('images/part3.jpg'),height: h,width: w,),);
+                                    },
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                  new MatrixGestureDetector(
+                    onMatrixUpdate: (m, tm, sm, rm) {
+                      notifier4.value = m;
+                    },
+                    // shouldRotate: false,
+                    // shouldScale: true,
+                    // shouldTranslate: false,
+                    child: AnimatedBuilder(
+                      animation: notifier4,
+                      builder: (ctx, child) {
+                        return Transform(
+                          transform: notifier4.value,
+                          child: Stack(
+                            children: <Widget>[
+                              Align(
+                                alignment: Alignment(0.0,0.65),
+                                child: Container(
+                                  width: w,
+                                  height: h,
+                                  //color: Colors.purple,
+                                  child: DragTarget(
+                                    builder: (context, List<int> candidateData, rejectedData) {
+                                      print(candidateData);
+                                      return Center(child: Image(image: AssetImage('images/part4.jpg'),height: h ,width: w,),);
+                                    },
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                  new SpeedDial(
+                    animatedIcon: AnimatedIcons.menu_arrow,
+                    animatedIconTheme: IconThemeData(size: 24.0),
+                    closeManually: false,
+                    marginRight: 21.5,
+                    marginBottom: 23.5,
+                    curve: Curves.easeIn,
+                    overlayColor: Colors.black,
+                    overlayOpacity: 0.3,
+                    foregroundColor: Colors.white,
+                    backgroundColor: Colors.purple,
+                    //tooltip: "Speed Dial123",
+                    //closeManually: true,
+                    children: [
+                      SpeedDialChild(
+                        child: Icon(Icons.zoom_in, size: 30,),
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.purple,
+                        label: "Zoom In",
+                        labelStyle: TextStyle(fontSize: 14.5),
+                        onTap: zoomin,
+                      ),
+                      SpeedDialChild(
+                        child: Icon(Icons.zoom_out, size: 30,),
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.purple,
+                        label: "Zoom Out",
+                        labelStyle: TextStyle(fontSize: 14.5),
+                        onTap: zoomout,
+                      ),
+                    ],
+                  ),
                 ],
               ),
             ),
